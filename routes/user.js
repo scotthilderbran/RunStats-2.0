7bbb35c04d5d4b714c31cb5e7be73dfefb98ca54
Scott <scott.hilderbran@gmail.com>
2020-04-02 17:35:42 -0500

    added cors and more data to response objects

--- routes/user.js
+++ routes/user.js
@@ -17,7 +17,6 @@ router.get("/", (req, res) => {
     .catch(err => console.log(err));
 });
 
-
 router.post("/register", function(req, res) {
   console.log(req.body);
   if (!req.body.email || !req.body.password) {
@@ -30,7 +29,17 @@ router.post("/register", function(req, res) {
       userLName: req.body.lName,
       sex: req.body.sex
     })
-      .then(user => res.status(201).send(user))
+      .then(user => {
+        let token = jwt.sign(
+          JSON.parse(JSON.stringify(user)),
+          process.env.AUTH_SECRET,
+          { expiresIn: 86400 * 30 }
+        );
+        jwt.verify(token, process.env.AUTH_SECRET, function(err, data) {
+          console.log(err, data);
+        });
+        res.json({ success: true, token: "JWT " + token });
+      })
       .catch(error => {
         console.log(error);
         res.status(400).send(error);
@@ -60,14 +69,12 @@ router.post("/login", function(req, res) {
           jwt.verify(token, process.env.AUTH_SECRET, function(err, data) {
             console.log(err, data);
           });
-          res.json({ success: true, token: "JWT " + token });
+          res.json({ success: true, token: "JWT " + token, user: user });
         } else {
-          res
-            .status(401)
-            .send({
-              success: false,
-              msg: "Authentication failed. Wrong password."
-            });
+          res.status(401).send({
+            success: false,
+            msg: "Authentication failed. Wrong password."
+          });
         }
       });
     })
--- server.js
+++ server.js
@@ -1,24 +1,23 @@
-require('dotenv').config({path: __dirname + '/config.env'}) //Load enviromental variables
-const express = require('express');
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
+require("dotenv").config({ path: __dirname + "/config.env" }); //Load enviromental variables
+const express = require("express");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 const path = require("path");
 const fs = require("fs");
-const db = require('./database/config/database');
+const db = require("./database/config/database");
+const cors = require("cors");
 
+const user = require("./routes/user");
+const run = require("./routes/run");
 
-const user = require('./routes/user');
-const run = require('./routes/run');
+const passport = require("passport");
 
-const passport    = require('passport');
-
-require('./passport');
-
-
+require("./passport");
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 
 const PORT = process.env.PORT || 5000;
 
@@ -26,12 +25,8 @@ app.listen(PORT, console.log(`Server started on port:${PORT}`));
 console.log(process.env.DB_PASS);
 
 db.authenticate()
-    .then(()=> console.log("connected"))
-    .catch(()=> console.log("Error"))
+  .then(() => console.log("connected"))
+  .catch(() => console.log("Error"));
 
-app.use('/user', user);
-app.use('/run', run);
-
-
-
-
+app.use("/user", user);
+app.use("/run", run);
