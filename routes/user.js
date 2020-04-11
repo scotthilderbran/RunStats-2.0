ca2baf84b155f92251cf90be5548f744242a08f8
Scott <scott.hilderbran@gmail.com>
2020-04-10 19:43:20 -0500

    Added verification to request body and email not in use check. Improved error response messages

--- routes/user.js
+++ routes/user.js
@@ -9,12 +9,22 @@ const getToken = require("../helpers/getToken");
 router.post("/register", function (req, res) {
   //Add data to find if user email exists already or not and return it
   console.log("PUBLIC - user/register POST request");
-  if (!req.body.email || !req.body.password) {
-    res.status(400).send({ msg: "Please pass username and password." });
+  console.log(req.body);
+  if (
+    !req.body.email ||
+    !req.body.password ||
+    !req.body.fName ||
+    !req.body.lName ||
+    !req.body.sex ||
+    !req.body.age
+  ) {
+    res.status(400).send({ message: "Please enter all fields" });
+  } else if (req.body.age > 120 || req.body.age <= 0) {
+    res.status(400).send({ message: "Please age range between 1 and 120" });
   } else {
     User.findOne({
       where: {
-        id: userId,
+        email: req.body.email,
       },
       raw: true,
     }).then((user) => {
@@ -46,7 +56,7 @@ router.post("/register", function (req, res) {
       } else {
         res.status(401).send({
           success: false,
-          message: "Email in use",
+          message: "Please login, email already in use",
         });
       }
     });
@@ -55,45 +65,49 @@ router.post("/register", function (req, res) {
 
 router.post("/login", function (req, res) {
   console.log("PUBLIC - user/login POST request");
-  User.findOne({
-    where: {
-      email: req.body.email.toLowerCase().replace(/\s/g, ""),
-    },
-  })
-    .then((user) => {
-      if (!user) {
-        return res.status(401).send({
-          message: "Authentication failed. User not found.",
-        });
-      }
-      user.comparePassword(req.body.password, (err, isMatch) => {
-        if (isMatch && !err) {
-          var token = jwt.sign(
-            JSON.parse(JSON.stringify(user)),
-            process.env.AUTH_SECRET,
-            { expiresIn: 86400 * 30 }
-          );
-          jwt.verify(token, process.env.AUTH_SECRET, function (err, data) {
-            console.log(err, data);
-          });
-          res.json({
-            success: true,
-            token: "JWT " + token,
-            user: {
-              email: user.email,
-              userFName: user.userFName,
-              userLName: user.userLName,
-            },
-          });
-        } else {
-          res.status(401).send({
-            success: false,
-            message: "Authentication failed. Wrong email/password.",
+  if (!req.body.email || !req.body.password) {
+    res.status(400).send({ message: "Please enter all fields" });
+  } else {
+    User.findOne({
+      where: {
+        email: req.body.email.toLowerCase().replace(/\s/g, ""),
+      },
+    })
+      .then((user) => {
+        if (!user) {
+          return res.status(401).send({
+            message: "Authentication failed. User not found.",
           });
         }
-      });
-    })
-    .catch((error) => res.status(400).send(error));
+        user.comparePassword(req.body.password, (err, isMatch) => {
+          if (isMatch && !err) {
+            var token = jwt.sign(
+              JSON.parse(JSON.stringify(user)),
+              process.env.AUTH_SECRET,
+              { expiresIn: 86400 * 30 }
+            );
+            jwt.verify(token, process.env.AUTH_SECRET, function (err, data) {
+              console.log(err, data);
+            });
+            res.json({
+              success: true,
+              token: "JWT " + token,
+              user: {
+                email: user.email,
+                userFName: user.userFName,
+                userLName: user.userLName,
+              },
+            });
+          } else {
+            res.status(401).send({
+              success: false,
+              message: "Authentication failed. Wrong email/password.",
+            });
+          }
+        });
+      })
+      .catch((error) => res.status(400).send(error));
+  }
 });
 
 router.get(
@@ -139,26 +153,53 @@ router.post(
     if (token) {
       decoded = jwt.verify(token, process.env.AUTH_SECRET);
       const userId = decoded.id;
-      console.log(req.body);
-      console.log(userId);
-      User.update(
-        {
+      const email = decoded.email;
+      console.log("decoded email");
+      console.log(email);
+      User.findOne({
+        where: {
           email: req.body.email.toLowerCase().replace(/\s/g, ""),
-          password: req.body.password,
-          userFName: req.body.fName,
-          userLName: req.body.lName,
-          sex: req.body.sex,
-          age: req.body.age,
         },
-        {
-          where: { id: userId },
+        raw: true,
+      }).then((user) => {
+        if (!user || req.body.email === email) {
+          if (req.body.age > 120 || req.body.age <= 0) {
+            res
+              .status(400)
+              .send({ message: "Please age range between 1 and 120" });
+          } else {
+            User.update(
+              {
+                email: req.body.email.toLowerCase().replace(/\s/g, ""),
+                password: req.body.password,
+                userFName: req.body.fName,
+                userLName: req.body.lName,
+                sex: req.body.sex,
+                age: req.body.age,
+              },
+              {
+                where: { id: userId },
+              }
+            )
+              .then((user) => {
+                res.json({
+                  email: user.email,
+                  userFName: user.userFName,
+                  userLName: user.userLName,
+                  sex: user.sex,
+                  age: user.age,
+                });
+                res.status(200);
+              })
+              .catch((err) => console.log(err));
+          }
+        } else {
+          res.status(401).send({
+            success: false,
+            message: "Email already in use",
+          });
         }
-      )
-        .then(() => {
-          console.log("here");
-          res.sendStatus(200);
-        })
-        .catch((err) => console.log(err));
+      });
     } else {
       return res.status(403).send({ success: false, msg: "Unauthorized." });
     }
