e591e6e43b11bf18025289b90ad5740483123809
Scott <scott.hilderbran@gmail.com>
2020-04-14 12:51:17 -0500

    updated sequelize connection url

--- database/config/database.js
+++ database/config/database.js
@@ -1,14 +1,32 @@
-const Sequelize = require('sequelize');
+const Sequelize = require("sequelize");
 
-module.exports =  new Sequelize(process.env.DB_NAME, 'postgres', process.env.DB_PASS, {
-  host: process.env.DB_HOST,
-  dialect: 'postgres',
-  operatorsAliases: false,
+let sequelize = null;
 
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
-  },
-});
+if (process.env.DATABASE_URL) {
+  sequelize = new Sequelize(process.env.process.env.DATABASE_URL, {
+    dialect: "postgres",
+    protocol: "postgres",
+    host: "<heroku host>",
+    logging: true, //false
+  });
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    "postgres",
+    process.env.DB_PASS,
+    {
+      host: process.env.DB_HOST,
+      dialect: "postgres",
+      operatorsAliases: false,
+
+      pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000,
+      },
+    }
+  );
+}
+
+module.exports = sequelize;
--- server.js
+++ server.js
@@ -25,8 +25,8 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`Server started on port:${PORT}`));
 
 db.authenticate()
-  .then(() => console.log("connected"))
-  .catch(() => console.log("Error"));
+  .then(() => console.log("Success - Connected to database"))
+  .catch(() => console.log("Error - Could not connect to database"));
 
 app.use("/user", user);
 app.use("/run", run);
