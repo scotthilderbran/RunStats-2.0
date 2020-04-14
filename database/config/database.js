d6d536544830cddc87bddb9c73d18355a02153d4
Scott <scott.hilderbran@gmail.com>
2020-04-14 12:22:47 -0500

    changed gitignore

---
+++ database/config/database.js
@@ -0,0 +1,14 @@
+const Sequelize = require('sequelize');
+
+module.exports =  new Sequelize(process.env.DB_NAME, 'postgres', process.env.DB_PASS, {
+  host: process.env.DB_HOST,
+  dialect: 'postgres',
+  operatorsAliases: false,
+
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  },
+});
